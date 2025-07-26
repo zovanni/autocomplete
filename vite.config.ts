4c19d034929826800b2d2b9219066e9c622bc55f
Giovanni Di Maggio <dimaggiogiovanni@gmail.com>
2025-07-26 10:54:04 +0000

    chore(mg)

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: true,
+    proxy: {
+      "/api/wikipedia": {
+        target: "https://en.wikipedia.org",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/wikipedia/, ""),
+        secure: true,
+      },
+    },
   },
 });
