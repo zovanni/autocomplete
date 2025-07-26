a2c763f62f8421affdad47a1267d55518b0ebcc4
Giovanni Di Maggio <dimaggiogiovanni@gmail.com>
2025-07-26 09:33:04 +0000

    chore(vite): config.allowedHosts set to true

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["q8ysdv-5173.csb.app"],
+    allowedHosts: true,
   },
 });
